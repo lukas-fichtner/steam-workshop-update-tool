6f98059db8d72e7f31dde0ae17820c6c80348189
Xedon <xedon@arctic-network.com>
2019-07-28 15:31:29 +0200

    v0.0.1
    - first commit
    - tested / stable version

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-steam-workshop-update-tool
+# Steam-Workshop-Addon-Update-Tool
+
+Mit diesem tool könnt Ihr ganz einfach euere Steam Addons updaten.
+Ursprünglich wurde dieses Tool für einen DayZ SA Server erstellt.
+
+# Installation
+- steamcmd herunterladne
+- pfade anpassen
+- mods anpassen
---
+++ mod-update.py
@@ -0,0 +1,60 @@
+import os
+#Default Settings
+steamCmdPath = 'C:\\Users\\Administrator\\Desktop\\steamcmd\\'
+steamCmdExe = 'steamcmd.exe'
+steamAppId = '221100'
+finalModPath = 'C:\\Users\\Administrator\\Desktop\\ModUpdateTool\\'
+
+#Login
+steamUser = 'xedontv'
+steamPass = ''
+steamauth = ''
+
+#Mods to Download
+steamMods = [
+	['1767379407','@EinsamimWaldServerMod'],
+	['1623711988','@VanillaPlusPlusMap'],
+	['1665663702','@MoreGuns'],
+	['1612576045','@EasySigns'],
+	['1560819773','@UnlimitedStamina'],
+	['1559212036','@CF'],
+	['1635058618','@DayZ-Expansion-Chat'],
+	['1566911166','@MasssManyItemOverhaul'],
+	['1574054508','@BuildAnywhere'],
+	['1571965849','@DisableBaseDestruction'],
+	['1667135157','@SimpleRoof'],
+	['1646187754','@CodeLock'],
+	['1582756848','@ZomBerryAdminTools'],
+	['1725477270','@GraveCross'],
+	['1680019590','@Server_Information_Panel'],
+]
+def fnc_modDownload(ModId, ModDir):
+	steamModId = ModId
+	finalModDir = ModDir
+	cmdCommand = (steamCmdPath+steamCmdExe+' +login '+steamUser+' '+steamPass+' '+steamauth+' +"workshop_download_item '+steamAppId+' '+steamModId+'" validate +quit')
+	symbolicLink = ('mklink /J "'+finalModPath+finalModDir+'" "'+steamCmdPath+'steamapps\\workshop\\content\\'+steamAppId+'\\'+steamModId+'"')
+	print(
+		'-- Verify final command --\n'
+		'AppId: '+steamAppId+'\n'
+		'ModId: '+steamModId+'\n'
+		'Mod Directory: '+finalModDir+'\n'
+		'SteamCmd command: '+cmdCommand+'\n'
+		'Symbolic link command: '+symbolicLink+'\n'
+		'--- End of verification ---\n'
+	)
+	#Ask if the Update should be started
+	runCode = input('Continue? y or n: ')
+	if runCode == 'y':
+		os.system(cmdCommand)
+		symLink = input('Create symbolic link? y or n (default: n): ')
+		#Ask if a SymbolicLink should be created
+		if symLink == 'y':
+			os.system(symbolicLink)
+			print('Symbolic link created \n')
+		else:
+			print('No symbolic link created \n')
+	else:
+		print('No Update')
+for Mods in steamMods:
+	fnc_modDownload(*Mods)
+input('Hit a key to exit')
